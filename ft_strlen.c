aefe041f9410094dc7cf08e597d59ccd9798a721
Changhyeon Lee <changhyl@student.42seoul.kr>
2022-12-13 19:47:25 +0900

    1

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: changhyl <changhyl@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 16:28:05 by changhyl          #+#    #+#             */
-/*   Updated: 2022/11/22 17:49:42 by changhyl         ###   ########.fr       */
+/*   Updated: 2022/12/13 19:47:19 by changhyl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,11 @@
 size_t	ft_strlen(const	char *s)
 {
 	int	len;
-	int	i;
 
 	len = 0;
-	i = 0;
-	while (*(s + i) != '\0')
+	while (*(s + len) != '\0')
 	{
 		len++;
-		i++;
 	}
 	return (len);
 }
